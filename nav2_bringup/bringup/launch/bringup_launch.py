4b7f6838904ca478725e9d5d287b0680615e39f3
Noppanut Thongton <nptttn.pat@gmail.com>
2021-11-01 21:45:20 +0700

    Edit for Angus

--- nav2_bringup/bringup/launch/bringup_launch.py
+++ nav2_bringup/bringup/launch/bringup_launch.py
@@ -60,6 +60,7 @@ def generate_launch_description():
 
     declare_map_yaml_cmd = DeclareLaunchArgument(
         'map',
+        default_value='/home/skuba_foxy/map.yaml',
         description='Full path to map yaml file to load')
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
